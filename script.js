b2f2b9015d2d8b861eb2557ea986fae8e02c445a
Henrique Martins <hiikemartins@gmail.com>
2023-11-13 23:18:43 -0300

    Chore: Primeiro Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const urlAPI = `https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
+const apiKey = `7b50a871-98b0-450a-b19b-d8941f11bc0c`;
+
+// funcao para pegar os valores das cripto moedas
+
+const getCriptoValues = async () => {
+  const criptos = [];
+  const trading = [];
+};
+
+const convertCriptoToCoin = (cripto, coin) => {
+  // logica para converter o valor da cripto para moeda desejada
+  return;
+};
