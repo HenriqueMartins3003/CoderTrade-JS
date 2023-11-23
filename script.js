b4f95a1c8cd6c0f9ec0c88e434c8c09b44df0025
Henrique Martins <hiikemartins@gmail.com>
2023-11-22 23:02:59 -0300

    Feat: adicionei funcao para bucr valores de cripto na API CoinApi

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-const urlAPI = `https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
-const apiKey = `7b50a871-98b0-450a-b19b-d8941f11bc0c`;
+// const urlAPI = `https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
+// const apiKey = `7b50a871-98b0-450a-b19b-d8941f11bc0c`;
 
-// funcao para pegar os valores das cripto moedas
+async function getBitcoinPriceAndExchangeRate(apiKey) {
+  const apiUrl = "https://rest.coinapi.io/v1/exchangerate/BTC";
+  const headers = {
+    "X-CoinAPI-Key": apiKey,
+  };
 
-const getCriptoValues = async () => {
-  const criptos = [];
-  const trading = [];
-};
+  try {
+    // Fetch Bitcoin price in USD
+    const priceResponse = await fetch(`${apiUrl}/USD`, { headers });
+    const priceData = await priceResponse.json();
+    const bitcoinPriceUSD = priceData.rate;
 
-const convertCriptoToCoin = (cripto, coin) => {
-  // logica para converter o valor da cripto para moeda desejada
-  return;
-};
+    // Fetch Bitcoin exchange rates against other currencies
+    const exchangeRateResponse = await fetch(apiUrl, { headers });
+    const exchangeRateData = await exchangeRateResponse.json();
+    const exchangeRates = exchangeRateData.rates;
+
+    return {
+      bitcoinPriceUSD,
+      exchangeRates,
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+// Example usage:
+const apiKey = "8C8FFECD-9008-43A8-A0ED-AF6F85DE8AED"; // Replace with your actual API key
+getBitcoinPriceAndExchangeRate(apiKey)
+  .then((data) => {
+    console.log("Bitcoin Price (USD):", data.bitcoinPriceUSD);
+    console.log("Exchange Rates:", data.exchangeRates);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
