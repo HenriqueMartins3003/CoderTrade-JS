d2efef1a93833a1790e39d77b4751195e0d2d454
Henrique Martins <hiikemartins@gmail.com>
2023-11-28 12:04:00 -0300

    novas funcoes handleclickexchange e getcripto prices

--- script.js
+++ script.js
@@ -11,45 +11,61 @@ const trxElement = document
   .getElementById("Lista4")
   .getAttribute("data-currency");
 
-  
+const getCriptoPrices = async (cripto) => {
+  const response = await fetch("https://api.coincap.io/v2/assets");
+
+  const data = await response.json();
+  const price = data.data;
+
+  const currency = price.find((item) => item.symbol == cripto);
+  //console.log(currency);
+  //console.log(currency.priceUsd);
+  return currency.priceUsd;
+};
+
+const handleclickExchange = async () => {
+  const screenCurrency = document.getElementById("criptoPrice");
+  const cripto = await getCriptoPrices(ethElement);
+  console.log(`Cripto: ${cripto} Screen: ${screenCurrency}`);
+  screenCurrency.innerHTML = `jkcjsdklvj;kvopf`;
+};
+
 // Lista de Criptos
 
-fetch("https://api.coincap.io/v2/assets").then(retorno =>{
-  return retorno.json()
-}).then(corpo => {
-  const Bitcoin = corpo.data[0]
+fetch("https://api.coincap.io/v2/assets")
+  .then((retorno) => {
+    return retorno.json();
+  })
+  .then((corpo) => {
+    const Bitcoin = corpo.data[0];
 
-    let price_BTC = Bitcoin["priceUsd"]
-    let cap_BTC = Bitcoin["marketCapUsd"]
+    let price_BTC = parseFloat(Bitcoin["priceUsd"]).toFixed(2);
+    let cap_BTC = parseFloat(Bitcoin["marketCapUsd"]).toFixed(2);
 
-    document.getElementById("price_BTC").innerHTML = price_BTC
-    document.getElementById("cap_BTC").innerHTML = cap_BTC
-//////////////////////////////////////////////////////////////////////
-  const Ethereum  = corpo.data[1]
-    let price_ETH = Ethereum["priceUsd"]
-    let cap_ETH = Ethereum["marketCapUsd"]
+    document.getElementById("price_BTC").innerHTML = `$ ${price_BTC}`;
+    document.getElementById("cap_BTC").innerHTML = `$ ${cap_BTC}b`;
+    //////////////////////////////////////////////////////////////////////
+    const Ethereum = corpo.data[1];
+    let price_ETH = parseFloat(Ethereum["priceUsd"]).toFixed(2);
+    let cap_ETH = parseFloat(Ethereum["marketCapUsd"]).toFixed(2);
 
-    document.getElementById("price_ETH").innerHTML = price_ETH
-    document.getElementById("cap_ETH").innerHTML = cap_ETH
+    document.getElementById("price_ETH").innerHTML = `$ ${price_ETH}`;
+    document.getElementById("cap_ETH").innerHTML = `$ ${cap_ETH}b`;
 
-////////////////////////////////////////////////////////////////////
-  const Litecoin = corpo.data[16]
+    ////////////////////////////////////////////////////////////////////
+    const Litecoin = corpo.data[16];
 
-    let price_Lit = Litecoin["priceUsd"]
-    let cap_Lit = Litecoin["marketCapUsd"]
+    let price_Lit = parseFloat(Litecoin["priceUsd"]).toFixed(2);
+    let cap_Lit = parseFloat(Litecoin["marketCapUsd"]).toFixed(2);
 
-    document.getElementById("price_Lit").innerHTML = price_Lit
-    document.getElementById("cap_Lit").innerHTML = cap_Lit
+    document.getElementById("price_Lit").innerHTML = `$ ${price_Lit}`;
+    document.getElementById("cap_Lit").innerHTML = `$ ${cap_Lit}b`;
 
-  const tron = corpo.data[9]
+    const tron = corpo.data[9];
 
-    let price_tron = tron["priceUsd"]
-    let cap_tron = tron["marketCapUsd"]
+    let price_tron = parseFloat(tron["priceUsd"]).toFixed(2);
+    let cap_tron = parseFloat(tron["marketCapUsd"]).toFixed(2);
 
-    document.getElementById("price_tron").innerHTML = price_tron
-    document.getElementById("cap_tron").innerHTML = cap_tron 
-
-  
-
-
-})
+    document.getElementById("price_tron").innerHTML = `$ ${price_tron}`;
+    document.getElementById("cap_tron").innerHTML = `$ ${cap_tron}b`;
+  });
