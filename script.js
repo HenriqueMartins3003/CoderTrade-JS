14a6946319f22e6166e6f8ca686ed7dfdc671c26
Gabriel Durante <gabrielhdurante@iclou.com>
2023-12-04 16:53:08 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const urlAPI = `https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
 const apiKey = `7b50a871-98b0-450a-b19b-d8941f11bc0c`;
 
-// funcao para pegar os valores das cripto moedas
-
 const getCriptoValues = async () => {
-  const criptos = [];
-  const trading = [];
+  try {
+    const response = await fetch(urlAPI, {
+      headers: {
+        'X-CMC_PRO_API_KEY': apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Erro ao obter os dados das criptomoedas');
+    }
+
+    const { data } = await response.json();
+    const criptos = data || []; 
+
+    return { criptos, trading: [] }; 
+  } catch (error) {
+    console.error(error);
+    return { criptos: [], trading: [] }; 
+  }
 };
 
 const convertCriptoToCoin = (cripto, coin) => {
-  // logica para converter o valor da cripto para moeda desejada
+  
   return;
 };
+
+
+getCriptoValues().then(({ criptos, trading }) => {
+  console.log('Criptomoedas:', criptos);
+  console.log('Trading:', trading);
+});
